c5a4aad16f71d8c2310dc20aa225c495b6efb13e
Anusha Malik <73341271+saymeowmeow@users.noreply.github.com>
2021-05-18 20:48:44 +0530

    Add files via upload

---
+++ htdocs/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <a class="navbar-brand" href="index.php" style="color : #E6858B;"><b>RESERVE BANK</b></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseNavbar">
+        <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+        <a class="nav-link" href="index.php" style="color: #E6858B;"><b> Home</b> </a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="createuser.php" style="color : #E6858B;"><b>Create User</b></a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="transfermoney.php" style="color : #E6858B;"><b>Transfer Money</b></a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="transactionhistory.php" style="color : #E6858B;"><b>Transaction History</b></a>
+</li>
+</div>
+</nav>
